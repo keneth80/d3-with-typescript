19ff802c3df399bdf7c76b5f8746ed5562d0be77
kenneth <pretty9967@naver.com>
2020-02-19 16:19:32 +0900

    modify canvas plot

--- src/component/series/basic-canvas-scatter-plot.ts
+++ src/component/series/basic-canvas-scatter-plot.ts
@@ -1,4 +1,4 @@
-import { Selection, BaseType, select } from 'd3-selection';
+import { Selection, BaseType, select, mouse } from 'd3-selection';
 import { quadtree } from 'd3-quadtree';
 import { Subject, Observable } from 'rxjs';
 
@@ -25,89 +25,134 @@ export class BasicCanvasScatterPlotModel {
 
 export interface BasicCanvasScatterPlotConfiguration {
     selector?: string;
-    canvasClass?: string;
 }
 
 export class BasicCanvasScatterPlot extends SeriesBase {
-    protected canvas: Selection<BaseType, any, HTMLElement, any>;
-
     private selector: string = 'basic-scatter';
 
-    private canvasClass: string = 'basic-scatter-plot';
+    private selectedPoint: [number, number];
+
+    private indexing: any = {};
+
+    protected canvas: Selection<BaseType, any, HTMLElement, any>;
+
+    protected pointerCanvas: Selection<BaseType, any, HTMLElement, any>;
 
     constructor(configuration: BasicCanvasScatterPlotConfiguration) {
         super();
-        if (configuration) {
-            if (configuration.selector) {
-                this.selector = configuration.selector;
-            }
-
-            if (configuration.canvasClass) {
-                this.canvasClass = configuration.canvasClass;
-            }
+        if (configuration.selector) {
+            this.selector = configuration.selector;
         }
     }
 
-    setSvgElement(svg: Selection<BaseType, any, HTMLElement, any>, 
-                  mainGroup: Selection<BaseType, any, HTMLElement, any>) {
+    setSvgElement(svg: Selection<BaseType, any, HTMLElement, any>) {
         this.svg = svg;
-        if (!mainGroup.select(`.${this.selector}-group`).node()) {
-            this.mainGroup = mainGroup.append('g').attr('class', `${this.selector}-group`);
+        select((this.svg.node() as HTMLElement).parentElement)
+            .select('svg')
+            .style('z-index', 1)
+            .style('position', 'absolute');
+        if (!this.canvas) {            
+            this.canvas = select((this.svg.node() as HTMLElement).parentElement)
+                .append('canvas')
+                .style('z-index', 2)
+                .style('position', 'absolute');
         }
 
-        this.canvas = select(this.canvasClass);
+        if (!this.pointerCanvas) {
+            this.pointerCanvas = select((this.svg.node() as HTMLElement).parentElement)
+                .append('canvas')
+                .style('z-index', 3)
+                .style('position', 'absolute');
+        }
     }
 
     drawSeries(chartData: Array<any>, scales: Array<Scale>, width: number, height: number) {
         const x: any = scales.find((scale: Scale) => scale.orinet === 'bottom').scale;
         const y: any = scales.find((scale: Scale) => scale.orinet === 'left').scale;
 
-        const quadTree = quadtree(chartData);
+        const quadTreeObj: any = quadtree()
+            .extent([[-1, -1], [width + 1, height + 1]])
+            .addAll(chartData.map((d: BasicCanvasScatterPlotModel) => [d.x, d.y]));
 
-        const pointRadius = 6;
+        const pointRadius = 4;
         
-        let active: any;
-        
-        this.mainGroup
-            .attr('transform', `translate( ${this.chartBase.chartMargin.left}, ${this.chartBase.chartMargin.top})`);
         this.canvas
             .attr('width', width - 1)
             .attr('height', height - 1)
             .style('transform', `translate(${(this.chartBase.chartMargin.left + 1)}px, ${(this.chartBase.chartMargin.top + 1)}px)`);
 
+        this.pointerCanvas
+            .attr('width', width - 1)
+            .attr('height', height - 1)
+            .style('transform', `translate(${(this.chartBase.chartMargin.left + 1)}px, ${(this.chartBase.chartMargin.top + 1)}px)`);
+
         const context = (this.canvas.node() as any).getContext('2d');
-        
+
+        const pointerContext = (this.pointerCanvas.node() as any).getContext('2d');
+        pointerContext.strokeStyle = 'white';
+        pointerContext.fillStyle = 'red';    
 
         context.clearRect(0, 0, width, height);
         context.fillStyle = 'steelblue';
         context.strokeWidth = 1;
         context.strokeStyle = 'white';
 
-        chartData.forEach((point: BasicCanvasScatterPlotModel) => {
-            if (!point.selected) {
-                this.drawPoint(point, pointRadius, x, y, context);
-            } else {
-                active = point;
+        this.pointerCanvas.on('click', () => {
+            const mouseEvent = mouse(this.pointerCanvas.node() as any);
+    
+            const xClicked = x.invert(mouseEvent[0]);
+            const yClicked = y.invert(mouseEvent[1]);
+
+            const closest = quadTreeObj.find(xClicked, yClicked);
+
+            const dX = x(closest[0]);
+            const dY = y(closest[1]);
+
+            const distance = this.euclideanDistance(mouseEvent[0], mouseEvent[1], dX, dY);
+
+            pointerContext.clearRect(0, 0, width, height);
+
+            if(distance < pointRadius) {
+                this.selectedPoint = closest;
+                const selectedIndex = this.indexing[this.selectedPoint[0] + '.' + this.selectedPoint[1]];
+                if (this.selectedPoint) {
+                    this.drawPoint(new BasicCanvasScatterPlotModel(this.selectedPoint[0], this.selectedPoint[1], 0, true), pointRadius, x, y, pointerContext);
+                }
             }
         });
 
-        // ensure that the actively selected point is drawn last
-        // so it appears at the top of the draw order
-        if (active) {
-            context.fillStyle = 'red';
-            this.drawPoint(active, pointRadius, x, y, context);
-            context.fillStyle = 'steelblue';
-        }
+        // TODO: 3단계에 걸쳐서 그리기.
+        chartData.forEach((point: BasicCanvasScatterPlotModel, index: number) => {
+            this.indexing[point.x + '.' + point.y] = index;
+            this.drawPoint(point, pointRadius, x, y, context);
+        });
+    }
+
+    search(quadtree: any, x0: number, y0: number, x3: number, y3: number) {
+        quadtree.visit((node: any, x1: number, y1: number, x2: number, y2: number) => {
+            if (!node.length) {
+                do {
+                    const d = node.data;
+                    d.scanned = true;
+                    d.selected = (d[0] >= x0) && (d[0] < x3) && (d[1] >= y0) && (d[1] < y3);
+                } while (node = node.next);
+            }
+            return x1 >= x3 || y1 >= y3 || x2 < x0 || y2 < y0;
+        });
+    }
+
+    nodes(quadtree: any) {
+        const nodes = [];
+        quadtree.visit((node: any, x1: number, y1: number, x2: number, y2: number) => {
+            nodes.push({x: x1, y: y1, width: x2 - x1, height: y2 - y1});
+        });
+        return nodes;
     }
 
     drawPoint(point: BasicCanvasScatterPlotModel, r: number, xScale: any, yScale: any, context: any) {
         const cx = xScale(point.x);
         const cy = yScale(point.y);
 
-        // NOTE; each point needs to be drawn as its own path
-        // as every point needs its own stroke. you can get an insane
-        // speed up if the path is closed after all the points have been drawn
-        // and don't mind points not having a stroke
         context.beginPath();
         context.arc(cx, cy, r, 0, 2 * Math.PI);
         context.closePath();
