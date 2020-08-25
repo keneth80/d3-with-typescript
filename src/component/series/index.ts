246ec0eb5aba2d9134e581d06921d5c304be670a
강효선 <kanghyoseon@ganghyoseon-ui-MacBook-Pro.local>
2020-08-25 10:13:50 +0900

    modify: modify series/index.ts(add canvas trace series)

--- src/component/series/index.ts
+++ src/component/series/index.ts
@@ -1,5 +1,8 @@
 export * from './canvas/basic-canvas-line-series';
+export * from './canvas/basic-canvas-trace';
+export * from './canvas/basic-canvas-scatter-plot';
+
 export * from './svg/basic-line-series';
 export * from './svg/basic-topology';
-export * from './canvas/basic-canvas-scatter-plot';
+
 export * from './webgl/basic-canvas-webgl-line-series-one';
