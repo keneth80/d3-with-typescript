5feef430e774775416c65ffa27ae4697b4314855
kenneth-dev <ssuny9967@gmail.com>
2020-09-07 10:05:19 +0900

    modify: modify series index.ts

--- src/component/series/index.ts
+++ src/component/series/index.ts
@@ -3,6 +3,9 @@ export * from './canvas/basic-canvas-trace';
 export * from './canvas/basic-canvas-scatter-plot';
 
 export * from './svg/basic-line-series';
+export * from './svg/grouped-vertical-bar-series';
+export * from './svg/stacked-vertical-bar-series';
+export * from './svg/vertical-bar-series';
 export * from './svg/basic-topology';
 
 export * from './webgl/basic-canvas-webgl-line-series-one';
